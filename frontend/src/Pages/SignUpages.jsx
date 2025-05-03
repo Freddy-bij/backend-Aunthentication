0d40e947bbab80db3c2b0696e6bdc56884026a90
Freddy Bijanja <freddybijanja31@gmail.com>
2025-05-03 14:15:34 +0200

    add password strength metter comonent

--- frontend/src/Pages/SignUpages.jsx
+++ frontend/src/Pages/SignUpages.jsx
@@ -3,12 +3,15 @@ import { useState } from "react"
 import { CiUser } from "react-icons/ci";
 import { IoMailOutline } from "react-icons/io5";
 import { LuLockKeyhole } from "react-icons/lu";
+import {Link} from "react-router-dom"
+import PasswordStrengthMetter from "../Component/PasswordStrengthMetter";
+
 
 const SignUpages = () => {
 
   const [name, setName] = useState("")
   const [email, setEmail] = useState("")
-  const [password , setPAssword] = useState("")
+  const [password , setPassword] = useState("")
 
   const handleSignUp = (e) => {
     e.preventDefault()
@@ -19,22 +22,24 @@ const SignUpages = () => {
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
-      className="max-w-md py-4 px-2  bg-gray-900/30 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflor-hidden"
+      className="max-w-md py-4-   bg-gray-900/30 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflor-hidden"
     >
 
       <div>
         <h1
-          className="text-3xl  font-bold mb-6 text-center bg-gray-900/30-"
+          className="text-xl mt-6 text-green-400 font-bold mb-6 text-center "
         >
-          create Account
+          Create Account
         </h1>
       </div>
-      <form onSubmit={handleSignUp} className="flex flex-col gap-2">
+
+      <div className="mx-2">
+         <form onSubmit={handleSignUp} className="flex flex-col  gap-2">
 
         <div className=" flex items-center " >
           <CiUser className="absolute left-3 z-10 text-green-600" />
           <input
-            className="relative pl-8 bg-gray-900/30 outline-none text-gray-200 "
+            className="relative pl-8 bg-gray-900/30 py-2 outline-none text-gray-200 "
             type="text"
             placeholder="full name"
             value={name}
@@ -48,7 +53,7 @@ const SignUpages = () => {
         <div className=" flex items-center " >
           <IoMailOutline  className="absolute left-3 z-10 text-green-600" />
           <input
-            className="relative pl-8 bg-gray-900/30 outline-none text-gray-200 "
+            className="relative pl-8 bg-gray-900/30 py-2 outline-none text-gray-200 "
             type="email"
             placeholder="Enter Email"
             value={email}
@@ -62,18 +67,40 @@ const SignUpages = () => {
         <div className=" flex items-center " >
           < LuLockKeyhole className="absolute left-3 z-10 text-green-600" />
           <input
-            className="relative pl-8 bg-gray-900/30 outline-none text-gray-200 "
+            className="relative pl-8 bg-gray-900/30 py-2 outline-none text-gray-200 "
             type="password"
             placeholder="Enter Password"
             value={password}
-            onChange={(e) => setPAssword(e.target.value)}
+            onChange={(e) => setPassword(e.target.value)}
 
 
           />
 
         </div>
 
+       <PasswordStrengthMetter password={password}/>
+
+        <motion.button className="mt-5 w-full py-3 px-4 bg-green-700 text-white font-bold rounded-lg shadow-lg
+         hover:from-green-600 hover:to-emerald-700 focus:outline-none 
+         focus:ring-2 focus:ring-green-500 focus:ring-offset-2 
+         focus:ring-offset-gray-900 transition duration-200 "
+
+         whileHover={{scale:1.02}}
+         whileTap={{scale:0.98}}
+         type="submit"
+        >Sign Up</motion.button>
       </form>
+      </div>
+     
+
+      <div className=" px-8 py-4 bg-gray-900/50 mt-6 flex justify-center">
+
+      <p className="text-sm text-gray-400">  
+        Aldrady have an account?{" "}
+        <Link to="/login" className="text-green-400 hover:underline">Login</Link>
+      </p>
+
+      </div>
     </motion.div>
   )
 }
